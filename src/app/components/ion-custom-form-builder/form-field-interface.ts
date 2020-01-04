612e635f38367fc70b1a4ff9e8d15cef81b41928
Ronald Hove <rnldhv91@gmail.com>
2020-01-04 12:47:51 +0200

    Readme Update #13

--- src/app/components/ion-custom-form-builder/form-field-interface.ts
+++ src/app/components/ion-custom-form-builder/form-field-interface.ts
@@ -1,5 +1,25 @@
 import { AbstractControl, Validators } from '@angular/forms';
-
+/*
+* icon: ion-icon name
+*
+* title: Form field label
+*
+* formControlName: Used to identify the field by angular form builder
+*
+* value: Form field controller holds form input data
+*
+* validators: An array of validators ie [Validators.required, Validators.minLength(10) . . ] full list of form validators in angular docs
+*
+* type: i.e <input type="text"> | support 'email', 'number', 'password', 'text', 'tel'
+*
+* placeholder: Preset form field value
+*
+* formFieldType: Current version supports 'inline' and 'textarea'
+*
+* textAreaRowCount: The height in rows if formFieldType === 'textarea'
+*
+* errors: A boolean for whether or not form field has additional errors apart from validation errors i.e errors applied on a network callback
+*/
 export interface FormField {
     icon: string;
     title: string;
